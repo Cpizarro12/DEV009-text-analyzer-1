7d1419cd8b38c96fe269817d165bfc5b88be4922
Cpizarro12 <pizarro.constanza12@gmail.com>
2023-06-14 11:51:48 -0400

    JS terminado

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,23 +1,58 @@
 const analyzer = {  
-  getWordCount: (text) => {
+  getWordCount: (userInput) => {
+    return userInput.split(' ').length;
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    console.log("Estamos en el archivo analyzer.js",text)
+  
   },
-  getCharacterCount: (text) => {
+  getCharacterCount: (userInput) => {
+    return userInput.length
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getCharacterCountExcludingSpaces: (text) => {
+  getCharacterCountExcludingSpaces: (userInput) => {
+    const textWithouthSpaces = userInput.replace(/[^\w]+/g,'');
+    return textWithouthSpaces.length;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (userInput) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const wordLength = userInput.split(' ') //Se divide userInput en un array
+    let sum = 0 //se inicializa la variable en 0 para almacenar la suma de las longitudes
+    let totalWords = 0 //Se inicializa la variable en 0 para almacenar el numero total de palabras
+    for (let i=0; i< wordLength.length; i++){ //Se usa el ciclo for para recorrer todas las palabras en wordLength. En cada iteración se agrega la longitud de la palabra actual al valor sum y se aumenta el contador de totalWords
+      sum += wordLength[i].length;
+      totalWords++}
+    let prom = sum / totalWords //se calcula el promedio de la longitud de palabras dividiendo la suma de las longitudes en el numero de palabras
+    prom = prom.toFixed(2) //Se utiliza tofixed para determinar que se muestren solo 2 decimales.
+
+    return Number(prom) //se ocupa Number para que lo mostrado sea un numero
   },
-  getNumberCount: (text) => {
+  getNumberCount: (userInput) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const numRegEx = /\b\d+(\.\d+)?\b/g; //Se crea la expresión regular valida solo para numeros
+    const matches = userInput.match(numRegEx); //se utiliza match para buscar todas las ocurrencias de la expresión regular.
+    if(matches === null){  //si la variable es null retorna 0, en caso contrario sretorna la longitud.
+      return 0; 
+    }
+    else{
+      return Number(matches.length);
+    }
   },
-  getNumberSum: (text) => {
+  getNumberSum: (userInput) => {
+    const nums = userInput.match(/\b\d+(\.\d+)?\b/g)
+    if (nums === null) {
+     
+      return 0
+    } 
+    let sum=0
+    for (let i = 0 ; i < nums.length; i ++){
+      sum += parseFloat(nums[i])  
+    }
+  
+    
+    return sum 
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
-};
+
+  }}
+;
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import analyzer from './analyzer.js';
-console.log(analyzer.getWordCount("hola"))
+
+const userInput = document.querySelector('textarea')
+const textinner = document.querySelectorAll('li')
+
+userInput.addEventListener('keyup', function(event){
+  
+  textinner[0].innerHTML ='Caracteres: '+analyzer.getCharacterCount(userInput.value);
+  textinner[1].innerHTML ='Caracteres sin espacio: '+ analyzer.getCharacterCountExcludingSpaces(userInput.value);
+  textinner[2].innerHTML ='Palabras: '+ analyzer.getWordCount(userInput.value);
+  textinner[3].innerHTML ='Números: '+ analyzer.getNumberCount(userInput.value);
+  textinner[4].innerHTML ='Suma Números: '+ analyzer.getNumberSum(userInput.value);
+  textinner[5].innerHTML ='Promedio de Palabras: '+ analyzer.getAverageWordLength(userInput.value);
+  console.log(event)
+})
+
+
+
+//Botón limpiar
+const btnClear = document.getElementById('reset-button')
+
+btnClear.addEventListener('click',() =>{
+  document.querySelector("textarea").value =""
+  textinner[0].innerHTML = "Caracteres: "
+  textinner[1].innerHTML = "Caracteres sin espacio: "
+  textinner[2].innerHTML = "Palabras: "
+  textinner[3].innerHTML = "Promedio de Palabras: "
+  textinner[4].innerHTML = "Números: "
+  textinner[5].innerHTML = "Suma Números: "
+  
+  
+    
+    
+})
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
